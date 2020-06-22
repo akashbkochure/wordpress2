54f6df98b4a9fc6d5c6eec33d21c9ec3ecfa8542
Farhan <farhan.ayub@cloudways.com>
2020-06-22 17:30:28 +0000

    first commit

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/430479.cloudwaysapps.com/cvzudydxss/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/430479.cloudwaysapps.com/cvzudydxss/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '119e671ecb35c9bab2e06397c00918c6');
+	include_once('/home/430479.cloudwaysapps.com/cvzudydxss/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
